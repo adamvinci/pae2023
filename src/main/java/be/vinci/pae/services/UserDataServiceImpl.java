3d41ad669207d3ab366c181bcd8365869618108b
adam <adame.barhdadi@student.vinci.be>
2023-02-22 13:51:28 +0100

    fix(3) checkstyle

--- src/main/java/be/vinci/pae/services/UserDataServiceImpl.java
+++ src/main/java/be/vinci/pae/services/UserDataServiceImpl.java
@@ -62,15 +62,10 @@ public class UserDataServiceImpl implements UserDataService {
           if (set.getString(3).equals("null")) {
             return null;
           } else {
-            userDTO.setId(set.getInt(1));
-            userDTO.setPassword(set.getString(2));
-            userDTO.setNom(set.getString(3));
-            userDTO.setPrenom(set.getString(4));
-            userDTO.setImage(set.getString(5));
-            userDTO.setDateInscription(
-                set.getDate(6).toLocalDate());
-            userDTO.setRole(set.getString(7));
-            userDTO.setGsm(set.getString(8));
+            initUser(userDTO, email, set.getString(2), set.getString(3),
+                set.getString(4),
+                set.getString(5), set.getDate(6).toLocalDate(), set.getString(7),
+                set.getString(8), set.getInt(1));
           }
 
         }
@@ -102,7 +97,7 @@ public class UserDataServiceImpl implements UserDataService {
             initUser(userDTO, set.getString(1), set.getString(2), set.getString(3),
                 set.getString(4),
                 set.getString(5), set.getDate(6).toLocalDate(), set.getString(7),
-                set.getString(8));
+                set.getString(8), id);
           }
 
         }
@@ -112,26 +107,28 @@ public class UserDataServiceImpl implements UserDataService {
       System.out.println("\n" + e.getMessage().split("\n")[0] + "\n");
     }
     disconnect();
-    userDTO.setId(id);
     return userDTO;
   }
 
   /**
    * init a UserDto from resultset got from the database (to pass the cpd duplications check).
    *
-   * @param userDTO
-   * @param email
-   * @param password
-   * @param nom
-   * @param prenom
-   * @param image
-   * @param date
-   * @param role
-   * @param gsm
+   * @param userDTO  the user in which the data will be added
+   * @param email    the email we have to search in the database or the email corresponding to the
+   *                 id in the database
+   * @param id       the id we have to search in the database or the id corresponding to the email
+   *                 in the database
+   * @param password the password corresponding to the email/id in the database
+   * @param nom      the name corresponding to the email/id in the database
+   * @param prenom   the prenom corresponding to the email/id in the database
+   * @param image    the image corresponding to the email/id in the database
+   * @param date     the date corresponding to the email/id in the database
+   * @param role     the role corresponding to the email/id in the database
+   * @param gsm      the gsm corresponding to the email/id in the database
    */
   public void initUser(UserDTO userDTO, String email, String password, String nom, String prenom,
       String image,
-      LocalDate date, String role, String gsm) {
+      LocalDate date, String role, String gsm, int id) {
     userDTO.setEmail(email);
     userDTO.setPassword(password);
     userDTO.setNom(nom);
@@ -140,5 +137,6 @@ public class UserDataServiceImpl implements UserDataService {
     userDTO.setDateInscription(date);
     userDTO.setRole(role);
     userDTO.setGsm(gsm);
+    userDTO.setId(id);
   }
 }
