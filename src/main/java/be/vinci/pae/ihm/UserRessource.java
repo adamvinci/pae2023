a681d0fb273c12895c2721389ef3f3c7584dccc2
Barhdadi Adam <adame.barhdadi@student.vinci.be>
2023-02-28 15:29:50 +0100

    refactor checkstyle (4)

--- src/main/java/be/vinci/pae/ihm/UserRessource.java
+++ src/main/java/be/vinci/pae/ihm/UserRessource.java
@@ -12,80 +12,85 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response.Status;
 import org.glassfish.jersey.server.ContainerRequest;
 
 /**
- * UserRessource retrieve the request process by Grizzly and treat it.
+ * UserRessource retrieve the request  process by Grizzly and treat it.
  */
 @Singleton
 @Path("/auths")
 public class UserRessource {
 
-    private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
-    private final ObjectMapper jsonMapper = new ObjectMapper();
+  private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
+  private final ObjectMapper jsonMapper = new ObjectMapper();
 
-    @Inject
-    private UserUcc userUcc;
+  @Inject
+  private UserUcc userUcc;
 
-    /**
-     * Login by providing an email and a password.
-     *
-     * @param userCredentials contains the email and password
-     * @return a json object with a token(formed by the user id) the user id and the user email
-     */
-    @POST
-    @Path("login")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ObjectNode login(JsonNode userCredentials) {
-        if (!userCredentials.hasNonNull("email") || !userCredentials.hasNonNull("password")) {
-            throw new WebApplicationException("email or password required", Status.BAD_REQUEST);
-        }
-
-        String email = userCredentials.get("email").asText();
-        String password = userCredentials.get("password").asText();
-
-        UserDTO userDTO = userUcc.login(email, password);
-
-        if (userDTO == null) {
-            throw new WebApplicationException("bad credentials", Status.BAD_REQUEST);
-        }
-
-        String token;
-        try {
-            token = JWT.create().withIssuer("auth0")
-                    .withClaim("user", userDTO.getId()).sign(this.jwtAlgorithm);
-
-            return jsonMapper.createObjectNode()
-                    .put("token", token)
-                    .putPOJO("user", Json.filterPublicJsonView(userDTO, UserDTO.class));
-
-        } catch (Exception e) {
-            System.out.println("Unable to create token");
-            return null;
-        }
-
+  /**
+   * Login by providing an email and a password.
+   *
+   * @param userCredentials contains the email and password
+   * @return a json object with a token(formed by the user id) the user id and the user email
+   */
+  @POST
+  @Path("login")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public ObjectNode login(JsonNode userCredentials) {
+    if (!userCredentials.hasNonNull("email") || !userCredentials.hasNonNull("password")) {
+      throw new WebApplicationException("email or password required", Status.BAD_REQUEST);
     }
 
-    /**
-     * Path to retrieve the user from a token.
-     *
-     * @param request contains the token in the header
-     * @return the user contained in the token
-     */
-    @GET
-    @Path("user")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Authorize
-    public ObjectNode getUser(@Context ContainerRequest request) {
-        UserDTO userDTO = (UserDTO) request.getProperty("user");
-        return jsonMapper.createObjectNode()
-                .putPOJO("User", Json.filterPublicJsonView(userDTO, UserDTO.class));
+    String email = userCredentials.get("email").asText();
+    String password = userCredentials.get("password").asText();
 
+    UserDTO userDTO = userUcc.login(email, password);
+
+    if (userDTO == null) {
+      throw new WebApplicationException("bad credentials", Status.BAD_REQUEST);
     }
+
+    String token;
+    try {
+      token = JWT.create().withIssuer("auth0")
+          .withClaim("user", userDTO.getId()).sign(this.jwtAlgorithm);
+
+      return jsonMapper.createObjectNode()
+          .put("token", token)
+          .putPOJO("user", Json.filterPublicJsonView(userDTO, UserDTO.class));
+
+    } catch (Exception e) {
+      System.out.println("Unable to create token");
+      return null;
+    }
+
+  }
+
+  /**
+   * Path to retrieve the user from a token.
+   *
+   * @param request contains the token in the header
+   * @return the user contained in the token
+   */
+  @GET
+  @Path("user")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public ObjectNode getUser(@Context ContainerRequest request) {
+    UserDTO userDTO = (UserDTO) request.getProperty("user");
+    return jsonMapper.createObjectNode()
+        .putPOJO("User", Json.filterPublicJsonView(userDTO, UserDTO.class));
+
+  }
 
 }
--- src/test/java/be/vinci/pae/domain/UserUccTest.java
+++ src/test/java/be/vinci/pae/domain/UserUccTest.java
@@ -1,5 +1,8 @@
 package be.vinci.pae.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import be.vinci.pae.business.domaine.User;
 import be.vinci.pae.business.domaine.UserImpl;
 import be.vinci.pae.business.ucc.UserUcc;
@@ -12,85 +15,84 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 /**
  * UserUccTest.
  */
 class UserUccTest {
 
-    ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinderMock());
+  ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinderMock());
 
-    private UserUcc userUcc;
-    private UserDataService userDataService;
-    private User userSteven;
+  private UserUcc userUcc;
+  private UserDataService userDataService;
+  private User userSteven;
 
 
-    @BeforeEach
-    void setUp() {
-        userUcc = locator.getService(UserUcc.class);
-        userDataService = locator.getService(UserDataService.class);
-        userSteven = locator.getService(User.class);
-        userSteven = Mockito.mock(UserImpl.class);
+  @BeforeEach
+  void setUp() {
+    userUcc = locator.getService(UserUcc.class);
+    userDataService = locator.getService(UserDataService.class);
+    userSteven = locator.getService(User.class);
+    userSteven = Mockito.mock(UserImpl.class);
 
-        Mockito.when(userSteven.getId()).thenReturn(2);
-        Mockito.when(userSteven.getEmail()).thenReturn("steven.agbassah@student.vinci.be");
-        Mockito.when(userSteven.getRole()).thenReturn("aidant");
-        Mockito.when(userSteven.getPassword()).thenReturn("123*");
-        Mockito.when(userSteven.checkPassword("123*")).thenReturn(true);
+    Mockito.when(userSteven.getId()).thenReturn(2);
+    Mockito.when(userSteven.getEmail()).thenReturn("steven.agbassah@student.vinci.be");
+    Mockito.when(userSteven.getRole()).thenReturn("aidant");
+    Mockito.when(userSteven.getPassword()).thenReturn("123*");
+    Mockito.when(userSteven.checkPassword("123*")).thenReturn(true);
 
-        Mockito.when(userDataService.getOne(2)).thenReturn(userSteven);
-        Mockito.when(userDataService.getOne("steven.agbassah@student.vinci.be")).thenReturn(userSteven);
+    Mockito.when(userDataService.getOne(2)).thenReturn(userSteven);
+    Mockito.when(userDataService.getOne("steven.agbassah@student.vinci.be")).thenReturn(userSteven);
 
-    }
+  }
 
-    @DisplayName("Test login(String email, String password) with good email and good password")
-    @Test
-    void testLoginWithGoodCredentials() {
+  @DisplayName("Test login(String email, String password) with good email and good password")
+  @Test
+  void testLoginWithGoodCredentials() {
 
-        assertEquals(userSteven, userUcc.login("steven.agbassah@student.vinci.be", "123*"));
-    }
+    assertEquals(userSteven, userUcc.login("steven.agbassah@student.vinci.be", "123*"));
+  }
 
-    @DisplayName("Test login(String email, String password) with good email and bad password")
-    @Test
-    void testLoginWithGoodEmailAndBadPassword() {
-        assertNull(userUcc.login("steven.agbassah@student.vinci.be", "123"), "Mauvais mot de passe");
+  @DisplayName("Test login(String email, String password) with good email and bad password")
+  @Test
+  void testLoginWithGoodEmailAndBadPassword() {
+    assertNull(userUcc.login("steven.agbassah@student.vinci.be", "123"), "Mauvais mot de passe");
 
-    }
+  }
 
-    @DisplayName("Test login(String email, String password) with bad email ")
-    @Test
-    void testLoginWithBadEmailAndGoodPassword() {
-        Mockito.when(userDataService.getOne("stevenagbassah@student.vinci.be"))
-                .thenReturn(null);
-        assertNull(userUcc.login("stevenagbassah@student.vinci.be", "123*"), "Cette email n'existe pas");
+  @DisplayName("Test  login(String email, String password) with bad email ")
+  @Test
+  void testLoginWithBadEmailAndGoodPassword() {
+    Mockito.when(userDataService.getOne("stevenagbassah@student.vinci.be"))
+        .thenReturn(null);
+    assertNull(userUcc.login("stevenagbassah@student.vinci.be", "123*"),
+        "Cette email n'existe pas");
 
-    }
+  }
 
-    @DisplayName("Test getOne(id) with the good id")
-    @Test
-    void getOneGoodId() {
-        assertEquals(userSteven, userUcc.getOne(2));
-    }
+  @DisplayName("Test getOne(id) with the good id")
+  @Test
+  void getOneGoodId() {
+    assertEquals(userSteven, userUcc.getOne(2));
+  }
 
-    @DisplayName("Verify the login page for both when the "
-            + "field is blank and submit button is clicked")
-    @Test
-    void testLoginWithBlankField() {
-        assertNull(userUcc.login("", ""), "champ login ou mots de passe sont vide");
-    }
+  @DisplayName("Verify the login page for both when the "
+      + "field is blank and submit button is clicked")
+  @Test
+  void testLoginWithBlankField() {
+    assertNull(userUcc.login("", ""), "champ login ou mots de passe sont vide");
+  }
 
-    @DisplayName("Verify if the email field is empty and password is completed")
-    @Test
-    void testLoginWithBlankUserName() {
-        assertNull(userUcc.login("", "123*"), "Completer le champ username");
-    }
+  @DisplayName("Verify if the email field is empty and password is completed")
+  @Test
+  void testLoginWithBlankUserName() {
+    assertNull(userUcc.login("", "123*"), "Completer le champ username");
+  }
 
-    @DisplayName("Verify if the email field is completed and the password field is empty")
-    @Test
-    void testLoginWithBlankPassword() {
-        assertNull(userUcc.login("steven.agbassah@student.vinci.be", " "), "Completer le champ password");
-    }
+  @DisplayName("Verify if the email field is completed and the password field is empty")
+  @Test
+  void testLoginWithBlankPassword() {
+    assertNull(userUcc.login("steven.agbassah@student.vinci.be", " "),
+        "Completer le champ password");
+  }
 
 }
