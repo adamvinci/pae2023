8d8924e41715ace26144f03b693f398254a56743
Ibrahim Bekkari <ibrahim.bekkari@student.vinci.be>
2023-03-05 21:17:43 +0100

    refactor: the visual aspect of my page

---
+++ webapp/src/Components/Pages/ObjetProposerPage.js
@@ -0,0 +1,6 @@
+const ObjetProposer = () => {
+  const main = document.querySelector('main');
+  main.innerHTML = `<h3>Page en cours de développement !</h3>`;
+};
+
+export default ObjetProposer;
--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -2,12 +2,18 @@ import HomePage from '../Pages/HomePage';
 import Logout from '../Logout/Logout';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
+import ObjetsEnVente from '../Pages/ObjetEnVentePage';
+import ObjetsVendu from '../Pages/ObjetVenduPage';
+import ObjetsProposer from '../Pages/ObjetProposerPage';
 
 const routes = {
   '/': HomePage,
   '/login': LoginPage,
   '/register': RegisterPage,
   '/logout': Logout,
+  '/ObjetEnVente': ObjetsEnVente,
+  '/ObjetVendu': ObjetsVendu,
+  '/ObjetProposer': ObjetsProposer,
 };
 
 export default routes;
