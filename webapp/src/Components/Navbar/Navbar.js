317f233a72d56560d841c8b85611d519f033421a
Elias <elias.zemmouri@student.vinci.be>
2023-03-11 18:51:00 +0100

    feat:ajout affichage par role && refactor:header

--- webapp/src/Components/Navbar/Navbar.js
+++ webapp/src/Components/Navbar/Navbar.js
@@ -11,13 +11,26 @@ const Navbar = () => {
 function header(){
 
   const h = document.querySelector('#head');
-  const head = `<div style="display: flex; justify-content: space-between;">
-    <div style="flex: 1; margin-top: 20px; float: left;">
-      <p>Rue de Heuseux 77ter<br>4671 BLEGNY</p>
+  const head = `
+  <div style="display: flex; flex-wrap: wrap; height: 10vh;">
+    <div style="flex-grow: 1;">
+      <div style="display: flex;">
+        <span style="background-color: #eee; padding: 20px;">
+          <p>Rue de Heuseux 77ter<br>4671 BLEGNY</p>
+        </span>
+      </div>
     </div>
-    <div style="flex: 50%; margin-top: 20px;   font-size: 50px">
-      <p>DonneMoi </p>
+    <div style="flex-grow: 1;">
+      <div style="display: flex; margin-left: 235px;">
+        <h1>
+          <span style="background-color: #eee; padding: 20px;">
+            DonneMoi
+          </span>
+        </h1>
+      </div>
     </div>
+    <div style="flex-grow: 1;"></div>
+  </div>
   
 `
 
@@ -44,7 +57,7 @@ async function renderNavbar() {
         </button>
         
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0" id='member'>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#" data-uri="/ObjetEnVente">Objets mis en vente</a>
             </li>      
@@ -60,60 +73,57 @@ async function renderNavbar() {
              <li id="registerItem" class="nav-item">
               <a class="nav-link" href="#" data-uri="/login">Login</a>
             </li> 
+            
           </ul>
-          <div id="member"></div>
+          
         </div>
       </div>
     </nav>
   </div>
 `;
 
-
-
   const navbar = document.querySelector('#navbarWrapper');
 
-  
-
   navbar.innerHTML = navadd;
-  if(!getToken()){
-    
-    navbar.innerHTML = navadd;
-    console.log("shesh");
-  }else{
-    console.log(authenticatedUser?.nom);
+
+  if(getToken()){
     const member=document.getElementById('member');
-    member.innerHTML=`
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    console.log(authenticatedUser?.role);
+    if(authenticatedUser?.role==='aidant' || authenticatedUser?.role==='Responsable'){
+    member.innerHTML+=`
+    
+    <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/Disponibilités">Disponibilités</a>
+    </li>
+    <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/ReceptionObjets">ReceptionObjets</a>
+    </li>
+    <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/RechercheObjets">RechercheObjets</a>
+    </li>
+    <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/TableauDeBord">TableauDeBord</a>
+    </li>
+    `;
+    }
+    if(authenticatedUser?.role==='Responsable'){
+      
+      member.innerHTML+=`
+    <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/ObjetsPropose">ObjetsPropose</a>
+    </li>
+    <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/Membres">Membres</a>
+    </li>
+    `;
+    }
+    member.innerHTML+=`
     <li class="nav-item">
               <a class="nav-link" href="#" data-uri="/logout">Logout</a>
     </li>
-    </ul>
     `;
-    if(authenticatedUser?.role==='aidant' || authenticatedUser?.role==='responsable'){
-      member.innerHTML+=`
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-              <a class="nav-link" href="#" data-uri="/logout">Aidant</a>
-    </li>
-    </ul>
-    `;
-    }
-    if(authenticatedUser?.role==='responsable'){
-      member.innerHTML+=`
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-              <a class="nav-link" href="#" data-uri="/logout">Responsable</a>
-    </li>
-    </ul>
-    `;
-    }
-   
-
   }
       
-  
-  // navbar.innerHTML = getToken() ? authenticatedUserNavbar : anonymousUserNavbar;
-  // <li class="nav-item"><a class="nav-link disabled" href="#">${authenticatedUser?.nom}</a></li>
 }
 
 export default Navbar;
---
+++ webapp/src/Components/Pages/Helper/Disponibilités.js
@@ -0,0 +1,6 @@
+const Disponibilités = () => {
+    const main = document.querySelector('main');
+    main.innerHTML = `<h3>Page en cours de développement !</h3>`;
+  };
+  
+  export default Disponibilités;
