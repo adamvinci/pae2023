07b44ea5db334252f7c1ca4f677a9f37f37e6d1f
Ibrahim Bekkari <ibrahim.bekkari@student.vinci.be>
2023-03-14 12:43:32 +0100

    refactor: objetProposees + init modifier etat objet in back

--- src/main/java/be/vinci/pae/dal/ObjectDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/ObjectDAOImpl.java
@@ -7,6 +7,7 @@ import be.vinci.pae.business.dto.UserDTO;
 import be.vinci.pae.business.factory.ObjetFactory;
 import be.vinci.pae.dal.services.DALService;
 import jakarta.inject.Inject;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,93 +20,115 @@ import java.util.List;
  */
 public class ObjectDAOImpl implements ObjectDAO {
 
-  @Inject
-  private DALService dalService;
+    @Inject
+    private DALService dalService;
 
-  @Inject
-  private ObjetFactory objetFactory;
+    @Inject
+    private ObjetFactory objetFactory;
 
-  @Inject
-  private UserDAO userDAO;
+    @Inject
+    private UserDAO userDAO;
 
-  @Inject
-  private TypeObjetDAO typeObjetDAO;
+    @Inject
+    private TypeObjetDAO typeObjetDAO;
 
-  @Inject
-  private DisponibiliteDAO disponibiliteDAO;
+    @Inject
+    private DisponibiliteDAO disponibiliteDAO;
 
-  @Override
-  public List<ObjetDTO> getAllObjet() {
-    List<ObjetDTO> objetDTOList = new ArrayList<>();
-    String query = "SELECT * FROM projet.objets";
-    try (PreparedStatement statement = dalService.preparedStatement(query)) {
-      try (ResultSet set = statement.executeQuery()) {
-        // check if resultset is empty (none objet)
-        if (!set.isBeforeFirst()) {
-          return null;
-        } else {
-          while (set.next()) {
-            ObjetDTO objetDTO = objetFactory.getObjet();
+    @Override
+    public List<ObjetDTO> getAllObjet() {
+        List<ObjetDTO> objetDTOList = new ArrayList<>();
+        String query = "SELECT * FROM projet.objets";
+        try (PreparedStatement statement = dalService.preparedStatement(query)) {
+            try (ResultSet set = statement.executeQuery()) {
+                // check if resultset is empty (none objet)
+                if (!set.isBeforeFirst()) {
+                    return null;
+                } else {
+                    while (set.next()) {
+                        ObjetDTO objetDTO = objetFactory.getObjet();
 
-            objetDTO.setIdObjet(Integer.parseInt(set.getString(1)));
-            UserDTO userDTO = userDAO.getOne(set.getString(2));
-            objetDTO.setUtilisateur(userDTO);
-            objetDTO.setGsm(set.getString(3));
-            TypeObjetDTO typeObjetDTO = typeObjetDAO.getOne(Integer.parseInt(set.getString(5)));
-            objetDTO.setTypeObjet(typeObjetDTO);
-            objetDTO.setDescription(set.getString(6));
-            DisponibiliteDTO disponibiliteDTO = disponibiliteDAO.getOne(
-                Integer.parseInt(set.getString(7)));
-            objetDTO.setDisponibilite(disponibiliteDTO);
-            objetDTO.setEtat(set.getString(8));
-            if (set.getString(9) != null) {
-              objetDTO.setDate_acceptation(LocalDate.parse(set.getString(9)));
+                        objetDTO.setIdObjet(Integer.parseInt(set.getString(1)));
+                        UserDTO userDTO = userDAO.getOne(set.getString(2));
+                        objetDTO.setUtilisateur(userDTO);
+                        objetDTO.setGsm(set.getString(3));
+                        TypeObjetDTO typeObjetDTO = typeObjetDAO.getOne(Integer.parseInt(set.getString(5)));
+                        objetDTO.setTypeObjet(typeObjetDTO);
+                        objetDTO.setDescription(set.getString(6));
+                        DisponibiliteDTO disponibiliteDTO = disponibiliteDAO.getOne(
+                                Integer.parseInt(set.getString(7)));
+                        objetDTO.setDisponibilite(disponibiliteDTO);
+                        objetDTO.setEtat(set.getString(8));
+                        if (set.getString(9) != null) {
+                            objetDTO.setDate_acceptation(LocalDate.parse(set.getString(9)));
+                        }
+                        objetDTO.setLocalisation(set.getString(10));
+                        if (set.getString(11) != null) {
+                            objetDTO.setDate_depot(LocalDate.parse(set.getString(11)));
+                        }
+                        if (set.getString(12) != null) {
+                            objetDTO.setDate_retrait(LocalDate.parse(set.getString(12)));
+                        }
+                        if (set.getString(13) != null) {
+                            objetDTO.setPrix(Double.parseDouble(set.getString(13)));
+                        }
+
+                        if (set.getString(14) != null) {
+                            objetDTO.setDate_vente(LocalDate.parse(set.getString(14)));
+                        }
+
+                        objetDTOList.add(objetDTO);
+                    }
+                }
             }
-            objetDTO.setLocalisation(set.getString(10));
-            if (set.getString(11) != null) {
-              objetDTO.setDate_depot(LocalDate.parse(set.getString(11)));
-            }
-            if (set.getString(12) != null) {
-              objetDTO.setDate_retrait(LocalDate.parse(set.getString(12)));
-            }
-            if (set.getString(13) != null) {
-              objetDTO.setPrix(Double.parseDouble(set.getString(13)));
-            }
-
-            if (set.getString(14) != null) {
-              objetDTO.setDate_vente(LocalDate.parse(set.getString(14)));
-            }
-
-            objetDTOList.add(objetDTO);
-          }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-    return objetDTOList;
-  }
-
-  @Override
-  public String getPicture(int id) {
-    String path = null;
-    String query = "SELECT photo FROM projet.objets WHERE id_objet = (?)";
-    try (PreparedStatement statement = dalService.preparedStatement(query)) {
-      statement.setInt(1, id);
-      try (ResultSet set = statement.executeQuery()) {
-        // check if resultset is empty (none objet)
-        if (!set.isBeforeFirst()) {
-          return null;
-        } else {
-          while (set.next()) {
-            path = set.getString(1);
-          }
-        }
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+        return objetDTOList;
     }
 
-    return path;
-  }
+    @Override
+    public String getPicture(int id) {
+        String path = null;
+        String query = "SELECT photo FROM projet.objets WHERE id_objet = (?)";
+        try (PreparedStatement statement = dalService.preparedStatement(query)) {
+            statement.setInt(1, id);
+            try (ResultSet set = statement.executeQuery()) {
+                // check if resultset is empty (none objet)
+                if (!set.isBeforeFirst()) {
+                    return null;
+                } else {
+                    while (set.next()) {
+                        path = set.getString(1);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return path;
+    }
+
+    public boolean modifierEtatObjet(String etat, int user,) {
+        String query = "SELECT * FROM projet.objets WHERE id_objet=";
+        try (PreparedStatement statement = dalService.preparedStatement(query)) {
+            try (ResultSet set = statement.executeQuery()) {
+                // check if resultset is empty (none objet)
+                if (!set.isBeforeFirst()) {
+                    return false;
+                } else {
+                    while (set.next()) {
+                        ObjetDTO objetDTO = objetFactory.getObjet();
+
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
 }
