f179890baab802f589596393321afc8cf5d581ac
Steven Agbassah <steven.agbassah@student.vinci.be>
2023-03-14 12:26:17 +0100

    feat : correction du frontend de la page /membres pour appeler l'api

--- webapp/src/Components/Pages/Responsable/Membres.js
+++ webapp/src/Components/Pages/Responsable/Membres.js
@@ -1,30 +1,30 @@
 const utilisateursTests = [
-  {nom : 'Agbassah', prenom : 'Steven', mail : 'steven.agbassah@student.vinci.be'},
-  {nom : 'Jacques', prenom : 'Jean', mail : 'jean.jacques@student.vinci.be'},
-  {nom : 'Agbassah', prenom : 'Roy', mail : 'Roy.agbassah@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
-  {nom : 'Bertrand', prenom : 'Amar', mail : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Agbassah', prenom : 'Steven', email : 'steven.agbassah@student.vinci.be'},
+  {nom : 'Jacques', prenom : 'Jean', email : 'jean.jacques@student.vinci.be'},
+  {nom : 'Agbassah', prenom : 'Roy', email : 'Roy.agbassah@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
+  {nom : 'Bertrand', prenom : 'Amar', email : 'bertrand.amar@student.vinci.be'},
   
 ];
 console.log(utilisateursTests);
 
-const renderUsersTable = () => {
+const renderUsersTable = async () => {
   const main = document.querySelector('main');
   const UsersTable = document.createElement('table');
   UsersTable.className = 'table';
@@ -39,13 +39,25 @@ const renderUsersTable = () => {
                           </thead>
                           <tbody>`  
   
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-  for (let index = 0; index < utilisateursTests.length; index+=1) {
+  const response = await fetch(`${process.env.API_BASE_URL}/users`, options);
+
+  if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+  const users = await response.json();
+
+  for (let index = 0; index < users.length; index+=1) {
       UsersTable.innerHTML += `<tr>
       <th scope="row">${index + 1}</th>
-      <td>${utilisateursTests[index].prenom}</td>
-      <td>${utilisateursTests[index].nom}</td>
-      <td>${utilisateursTests[index].mail}</td>
+      <td>${users[index].prenom}</td>
+      <td>${users[index].nom}</td>
+      <td>${users[index].email}</td>
       <td><button type="button" class="btn btn-success">Indiquer aidant</button></td>
   </tr>`
 
