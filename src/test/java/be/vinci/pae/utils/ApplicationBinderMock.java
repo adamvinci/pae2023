9e88e63cf7016633672b6e8ced0bbc5b7535d3ad
leon kelmendi <leon.kelmendi@student.vinic.be>
2023-03-23 18:42:50 +0100

    feat ajout threadLocal + transaction

--- src/test/java/be/vinci/pae/utils/ApplicationBinderMock.java
+++ src/test/java/be/vinci/pae/utils/ApplicationBinderMock.java
@@ -1,11 +1,13 @@
 package be.vinci.pae.utils;
 
 import be.vinci.pae.business.factory.UserFactory;
+import be.vinci.pae.dal.services.DALServiceImpl;
 import be.vinci.pae.business.factory.UserFactoryImpl;
 import be.vinci.pae.business.ucc.UserUcc;
 import be.vinci.pae.business.ucc.UserUccImpl;
 import be.vinci.pae.dal.UserDAO;
 import be.vinci.pae.dal.UserDAOImpl;
+import be.vinci.pae.dal.services.DALTransaction;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.ext.Provider;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
@@ -27,5 +29,6 @@ public class ApplicationBinderMock extends AbstractBinder {
     // but cant ass dev.propreties not initialzied
     bind(Mockito.mock(UserDAOImpl.class)).to(UserDAO.class);
     bind(UserFactoryImpl.class).to(UserFactory.class).in(Singleton.class);
+    bind(Mockito.mock(DALServiceImpl.class)).to(DALTransaction.class);
   }
 }
