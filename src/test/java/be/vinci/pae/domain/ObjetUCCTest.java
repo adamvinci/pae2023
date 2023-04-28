f1193ca0503d3206c41c13b1a043a13d2a6e37fc
Elias <elias.zemmouri@student.vinci.be>
2023-04-28 17:29:03 +0200

    refactor: ObjetUCCTest return of notificationDTO.getMessage() when objetUCC.refuserObjet

--- src/test/java/be/vinci/pae/domain/ObjetUCCTest.java
+++ src/test/java/be/vinci/pae/domain/ObjetUCCTest.java
@@ -255,7 +255,7 @@ class ObjetUCCTest {
     assertAll(
         () -> assertEquals(objetDTO, objetUCC.refuserObject(objetDTO, "message", notificationDTO)),
         () -> assertEquals(
-            "l'objet (" + objetDTO.getDescription() + ") est refusé car: " + "message",
+            "Votre Objet (" + objetDTO.getDescription() + ") a été refusé car: " + "message",
             notificationDTO.getMessage())
     );
   }
@@ -273,7 +273,7 @@ class ObjetUCCTest {
     assertAll(
         () -> assertEquals(objetDTO, objetUCC.refuserObject(objetDTO, "message", notificationDTO)),
         () -> assertEquals(
-            "l'objet (" + objetDTO.getDescription() + ") est refusé car: " + "message",
+            "Votre Objet (" + objetDTO.getDescription() + ") a été refusé car: " + "message",
             notificationDTO.getMessage())
     );
   }
